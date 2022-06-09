94b230eea63c906358606d942adec1da3960f6c2
Arunprashath S <arunprashaths@dpinfotech.lk>
2022-06-09 10:54:22 +0530

    nltk downloads

--- JDapp.py
+++ JDapp.py
@@ -8,7 +8,7 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 nltk.download('stopwords')
 nltk.download('wordnet')
-
+nltk.download('omw-1.4')
 from nltk.corpus import stopwords
 from nltk import PorterStemmer
 from nltk import WordNetLemmatizer
